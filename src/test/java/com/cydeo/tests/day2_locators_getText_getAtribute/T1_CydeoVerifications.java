e661fcb64ff8d162dd6f4f64452a1b5667a4f107
vahap iren <vahapiren@gmail.com>
2022-12-21 08:22:17 +0300

    day2:T1 Cydeo verifications

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAtribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAtribute/T1_CydeoVerifications.java
@@ -1,9 +1,34 @@
 package com.cydeo.tests.day2_locators_getText_getAtribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_CydeoVerifications {
     public static void main(String[] args) {
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com");
 
+        //verify URL contains "cydeo"
+        String expectedURL="cydeo";
+        String actualURL= driver.getCurrentUrl();
+
+        if (actualURL.contains(expectedURL))
+            System.out.println("URL verification PASSED");
+        else
+            System.out.println("URL verification FAILED");
+
+        //Verify title is "Practice"
+        String expectedTitle="Practice";
+        String actualTitle= driver.getTitle();
+        if (actualTitle.equals(expectedTitle))
+            System.out.println("Title verification PASSED");
+        else
+            System.out.println("Title verification FAILED");
+        driver.close();
 
 
     }
