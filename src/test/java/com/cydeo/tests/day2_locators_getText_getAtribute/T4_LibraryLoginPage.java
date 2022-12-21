38cdfe05e9432d1ac5915a90c07a5d6ac39052d9
vahap iren <vahapiren@gmail.com>
2022-12-21 10:30:25 +0300

    day2:T4 LibraryLogin Page

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAtribute/T4_LibraryLoginPage.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAtribute/T4_LibraryLoginPage.java
@@ -1,7 +1,27 @@
 package com.cydeo.tests.day2_locators_getText_getAtribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T4_LibraryLoginPage {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://library2.cybertekschool.com/login.html");
+
+        //enter user name:"incorrect@email.com"
+        WebElement userNameInput=driver.findElement(By.className("form-control"));
+        userNameInput.sendKeys("incorrect@email.com");
+
+        //enter password: incorrect password
+
+        //click to sign in button
+
 
 
 
