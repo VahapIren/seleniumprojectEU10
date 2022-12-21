e10f734b3733ba8c2ca763ef4ab1f87224d8e275
vahap iren <vahapiren@gmail.com>
2022-12-21 08:42:19 +0300

    day2:T2 LinkText Practice

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAtribute/T2_LinkTextPractice.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAtribute/T2_LinkTextPractice.java
@@ -3,6 +3,7 @@ package com.cydeo.tests.day2_locators_getText_getAtribute;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T2_LinkTextPractice {
@@ -16,10 +17,31 @@ public class T2_LinkTextPractice {
         driver.get("https://practice.cydeo.com");
 
         //click to A/B Testing from top of the list
-        driver.findElement(By.linkText("A/B Testing")).click();
+        //driver.findElement(By.linkText("A/B Testing")).click();
         //if we write wrong or not found NoSuchElementException comes
+        WebElement abTestLink=driver.findElement(By.linkText("A/B Testing"));
+        abTestLink.click();
 
         //verify title is "No A/B Test"
+        String expectedTitle="No A/B Test";
+        String actualTitle=driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle))
+            System.out.println("Title verification PASSED!");
+        else
+            System.out.println("Title verification FAILED!");
+
+        //Go back to home page by using .back()
+        driver.navigate().back();
+
+        //Verify title is "Practice"
+        String expectedTitle2="Practice";
+        actualTitle=driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle2))
+            System.out.println("Title verification PASSED!");
+        else
+            System.out.println("Title verification FAILED!");
 
 
     }
