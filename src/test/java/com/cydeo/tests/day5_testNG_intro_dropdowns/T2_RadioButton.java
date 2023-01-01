c64594f258f6598c88843aca8bc8656daa1aad3b
vahap iren <vahapiren@gmail.com>
2023-01-01 17:03:52 +0300

    day5: RadioButton

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
@@ -0,0 +1,36 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class T2_RadioButton {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        WebElement hockeyRadioBtn=driver.findElement(By.xpath("//input[@id='hockey']"));
+
+        Thread.sleep(2000);
+        hockeyRadioBtn.click();
+
+        System.out.println("hockeyRadioBtn.isSelected() = " + hockeyRadioBtn.isSelected());
+
+        if (hockeyRadioBtn.isSelected()){
+            System.out.println("Button is selected Verification Passed");
+        }else
+        System.out.println("Button is selected Verification Failed");
+
+        driver.close();
+
+
+
+    }
+}
