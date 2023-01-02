159289b4bf2481e2d67ee120dfa6053d8f3d5cc4
vahap iren <vahapiren@gmail.com>
2023-01-02 23:27:59 +0300

    day6: simple dropdown

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,6 +22,22 @@ public class T4_SimpleDropdowns {
     }
     @Test
     public void simpleDropdownTest(){
+        Select simpleDropdown=new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+        WebElement currentlySelectedOption =simpleDropdown.getFirstSelectedOption();
+        String actualSimpleDropdownText= currentlySelectedOption.getText();
+        System.out.println(actualSimpleDropdownText);
+        String expectedSimpleDropdownText="Please select an option";
+
+        Assert.assertEquals(actualSimpleDropdownText,expectedSimpleDropdownText);
+
+        Select stateDropdown=new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        //WebElement currentlySelectedOption2= stateDropdown.getFirstSelectedOption();
+        String expectedSteteDropdownText="Select a State";
+        String actualStateDropdownText=stateDropdown.getFirstSelectedOption().getText();
+        System.out.println(actualStateDropdownText);
+
+        Assert.assertEquals(actualStateDropdownText,expectedSteteDropdownText);
+
 
     }
 
